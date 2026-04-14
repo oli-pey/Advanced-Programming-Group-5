4738b40b4dd3a28b5add06e5ac30f19b48a6119f
Oliver Peyron <oliver.peyron@students.fhnw.ch>
2026-04-14 10:13:54 +0200

    Cleanup

--- test_recognizer.py
+++
@@ -1,9 +0,0 @@
-from ml.registry import get_recognizer
-
-with open("test_digits/my_digit7.png", "rb") as f:
-    image_bytes = f.read()
-
-recognizer = get_recognizer("cnn")
-result = recognizer.predict_from_png_bytes(image_bytes)
-
-print(result)
